cc51f57bf7550cf67f1aea998449cb71aa5c1ad1
Marcelo-Zometa <marzometa21@gmail.com>
2025-10-20 18:43:04 -0230

    #8 Users are able to logout

--- uni-chatbot/users/views.py
+++ uni-chatbot/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.urls import reverse
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 
 # Tutorial in https://www.youtube.com/watch?v=DIFaOkxy6TE&list=PL0Zuz27SZ-6NamGNr7dEqzNFEcZ_FAUVX&index=9
 
@@ -19,10 +19,14 @@ def register_view(request):
 def login_view(request):
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            #LOGIN HERE
+        if form.is_valid():            
             login(request, form.get_user())
             return redirect("/")
     else: 
         form = AuthenticationForm()
     return render(request, "users/login.html", {"form": form})
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("/")
