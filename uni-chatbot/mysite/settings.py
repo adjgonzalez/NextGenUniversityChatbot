d8416b16f47712733158cb0597a4ae330b8cd60d
Marcelo-Zometa <marzometa21@gmail.com>
2025-11-26 17:59:25 -0330

    #30 fix: Removed broken hasher
    Had to remove hashing package that was broken. Moved project to default django python packages for it

--- uni-chatbot/mysite/settings.py
+++ uni-chatbot/mysite/settings.py
@@ -149,7 +149,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 PASSWORD_HASHERS = [
-    "django.contrib.auth.haschers.Argon2PasswordHasher",
+    # "django.contrib.auth.hashers.Argon2PasswordHasher",
     "django.contrib.auth.hashers.PBKDF2PasswordHasher",
     "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
 ]
