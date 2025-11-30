d40ed702b2e52567892229bd1ee8483f9f66ba47
Nabil Hasan <nabilhasan468@gmail.com>
2025-11-30 17:01:17 -0330

    #12 fix: Removed the dead code Testing from mysite/settings.py

--- uni-chatbot/mysite/settings.py
+++ uni-chatbot/mysite/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Load environment variables from .env file
 load_dotenv(BASE_DIR / '.env')
-TESTING = 'test' in sys.argv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
