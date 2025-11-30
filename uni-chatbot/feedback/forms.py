d5c26e39b974abc8eb93acce67c7f649d73a8b98
adjgonzalez <adjgonzalez@mun.ca>
2025-11-30 18:50:22 -0330

    fix: fixed feedback test (submissions and old localization)

--- uni-chatbot/feedback/forms.py
+++ uni-chatbot/feedback/forms.py
@@ -1,34 +1,37 @@
 from django import forms
-from .models import Feedback, AnonymousFeedback
+
+from .models import AnonymousFeedback, Feedback
+
 
 class FeedbackForm(forms.ModelForm):
     class Meta:
         model = Feedback
-        exclude = ('user',)
-        
+        exclude = ("user",)
+
         widgets = {
-            'type': forms.Select(attrs={
-                'class': 'form-select form-select-lg'
-            }),
-            'message': forms.Textarea(attrs={
-                'class': 'form-control form-control-lg', 
-                'rows': 7,
-                'placeholder': 'Let us know how we can improve...'
-            }),
+            "type": forms.Select(attrs={"class": "form-select form-select-lg"}),
+            "message": forms.Textarea(
+                attrs={
+                    "class": "form-control form-control-lg",
+                    "rows": 7,
+                    "placeholder": "Let us know how we can improve...",
+                }
+            ),
         }
+
 
 class AnonymousFeedbackForm(forms.ModelForm):
     class Meta:
         model = AnonymousFeedback
-        exclude = ('user',)
-        
+        exclude = ("user",)
+
         widgets = {
-            'type': forms.Select(attrs={
-                'class': 'form-select form-select-lg'
-            }),
-            'message': forms.Textarea(attrs={
-                'class': 'form-control form-control-lg', 
-                'rows': 7,
-                'placeholder': 'Let us know how we can improve...'
-            }),
+            "type": forms.Select(attrs={"class": "form-select form-select-lg"}),
+            "message": forms.Textarea(
+                attrs={
+                    "class": "form-control form-control-lg",
+                    "rows": 7,
+                    "placeholder": "Let us know how we can improve...",
+                }
+            ),
         }
