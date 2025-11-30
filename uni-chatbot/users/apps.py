1df8e7fc258fdb1f514c8a17b80d3ac54e5d1d31
Marcelo-Zometa <marzometa21@gmail.com>
2025-11-29 23:38:43 -0330

    #18 feat: Added functioning email service to confirm user application

--- uni-chatbot/users/apps.py
+++ uni-chatbot/users/apps.py
@@ -1,10 +1,10 @@
 from django.apps import AppConfig
 
-
 class UsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "users"
 
     # Registering the signal to the app
     def ready(self):
-        pass
+        # Import signals so they get registered
+        import users.signals
