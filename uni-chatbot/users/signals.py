136286804dc431dcdc0f9961e603c1a688ed9c01
adjgonzalez <adjgonzalez@mun.ca>
2025-12-02 00:44:28 -0330

    fix: ran ruff now.

--- uni-chatbot/users/signals.py
+++ uni-chatbot/users/signals.py
@@ -1,9 +1,10 @@
+import logging
+
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .models import UserProfile
-import logging
 
 logger = logging.getLogger(__name__)
 
